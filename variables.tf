bb5ac639877c024c92df0afd479faa939832e8d1
Andres Riancho <andres.riancho@gmail.com>
2020-01-08 14:31:57 -0300

    Missing region variable definition

--- variables.tf
+++ variables.tf
@@ -1,13 +1,17 @@
 variable "mail_from" {
   description = "The 'from' email address to use when sending notifications"
-  default     = "ops-team@example.com"
+  default     = "it@ggvc.com"
 }
 
 variable "mail_to" {
   description = "The 'to' email address where notifications are sent"
-  default     = "ops-team@example.com"
+  default     = "it@ggvc.com"
 }
 
 variable "profile" {
   default = "terraform"
+}
+
+variable "region" {
+  default = "us-east-1"
 }
